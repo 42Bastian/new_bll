ac1b86ea737c0546b00aa59cc6d89e2242ad3ec6
42Bastian <bs42@monlynx.de>
2023-01-31 18:22:15 +0100

    fix for M1 Mac

--- sendobj.js/sendobj.js
+++ sendobj.js/sendobj.js
@@ -1,12 +1,21 @@
+#!/usr/bin/env node
+
 /* sendobj as node.js */
 
 const SerialPort = require('serialport');
 const fs = require('fs');
+const InterByteTimeoutParser = require('@serialport/parser-inter-byte-timeout')
+const  ByteLengthParser  = require('@serialport/parser-byte-length')
 var defaultPort = 'COM7'
 var defaultBaudrate = 62500
 
 var argc = process.argv.length - 2
 var index = 2
+
+function done()
+{
+   process.exit(0);
+}
 
 while ( argc > 1 ) {
     if ( process.argv[index] === '-p' && argc > 1 ){
@@ -35,6 +44,9 @@ port.on('error',function(err) {
     console.log('Error ', err.message)
 })
 
+if ( defaultBaudrate != 1000000 ){
+    const parser = port.pipe(new InterByteTimeoutParser({ interval: 1}))
+}
 
 try {
     var buffer = fs.readFileSync(file)
@@ -49,13 +61,20 @@ header[2] = buffer[2];          // destination low byte
 header[3] = buffer[3];          // destination high byte
 header[4] = (len >> 8) ^ 0xff   // length high byte prepared for inc
 header[5] = (len & 0xff) ^ 0xff // length low byte prepared for inc
+
 if ( !port.write(header, 'binary') ) {
     console.log("KO\n");
     return
 }
 
 buffer = buffer.slice(10);      // skip BLL header
-if ( !port.write(buffer, 'binary') ) {
+
+const parser = port.pipe(new ByteLengthParser({ length: len }))
+parser.on('data', done) // event when all bytes are written.
+
+var ok = port.write(buffer, 'binary')
+if ( !ok ) {
     console.log("KO\n");
-    return
+    process.exit(0);
 }
+port.drain();
